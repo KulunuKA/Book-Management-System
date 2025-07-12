e6056554e25370f6ff1363835836e9a0a5e80e5e
Kulunu Kasthuri <kulunukasthuri38@gmail.com>
2025-07-12 06:41:53 +0530

    feat: Update Sidebar component with new icons and logout functionality

--- frontend/src/components/navigations/Sidebar.jsx
+++ frontend/src/components/navigations/Sidebar.jsx
@@ -1,4 +1,4 @@
-"use client"; // Add this if you're in Next.js App Router
+"use client";
 
 import * as React from "react";
 import { useRouter, usePathname } from "next/navigation";
@@ -13,8 +13,9 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/Inbox";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ListIcon from "@mui/icons-material/List";
+import AddIcon from "@mui/icons-material/Add";
 import style from "./style.module.css";
 
 const drawerWidth = 240;
@@ -27,14 +28,21 @@ export default function Sidebar() {
     {
       name: "Create Book",
       path: "/book-form",
-      icon: <InboxIcon />,
+      icon: <AddIcon />,
     },
     {
       name: "View Books",
       path: "/book-list",
-      icon: <MailIcon />,
+      icon: <ListIcon />,
     },
   ];
+
+  const handleLogout = () => {
+    if (window.confirm("Are you sure you want to logout?")) {
+      sessionStorage.removeItem("TKN");
+      router.push("/login");
+    }
+  };
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -46,31 +54,50 @@ export default function Sidebar() {
           "& .MuiDrawer-paper": {
             width: drawerWidth,
             boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
           },
         }}
         variant="permanent"
         anchor="left"
       >
-        <Box sx={{ textAlign: "center", p: 2 }}>
-          <Typography variant="h6" noWrap component="div">
-            Book Management
-          </Typography>
+        <Box>
+          <Box sx={{ textAlign: "center", p: 2 }}>
+            <Typography variant="h6" noWrap component="div">
+              Book Management
+            </Typography>
+          </Box>
+          <Divider />
+          <List>
+            {tabs.map((tab) => (
+              <ListItem key={tab.name} disablePadding>
+                <ListItemButton
+                  onClick={() => router.push(tab.path)}
+                  selected={pathname === tab.path}
+                  className={pathname === tab.path ? style.active : ""}
+                >
+                  <ListItemIcon>{tab.icon}</ListItemIcon>
+                  <ListItemText primary={tab.name} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </Box>
-        <Divider />
-        <List>
-          {tabs.map((tab) => (
-            <ListItem key={tab.name} disablePadding>
-              <ListItemButton
-                onClick={() => router.push(tab.path)}
-                selected={pathname === tab.path}
-                className={pathname === tab.path ? style.active : ""}
-              >
-                <ListItemIcon>{tab.icon}</ListItemIcon>
-                <ListItemText primary={tab.name} />
+        {/* Logout Button at Bottom */}
+        <Box>
+          <Divider />
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                  <LogoutIcon />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
               </ListItemButton>
             </ListItem>
-          ))}
-        </List>
+          </List>
+        </Box>
       </Drawer>
     </Box>
   );
